cad100feef3b0f240fa3fe750fdab0bf5a76267e
Jakub Hartyniuk <kb.hartyniuk@gmail.com>
2017-08-05 18:22:00 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: ['whatwg-fetch', "./js/scripts.js"],
+  module: {
+      loaders: [
+        {
+            test: /\.js?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-0'],
+                plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+            }
+        }
+      ]
+  },
+  output: {
+    path: __dirname + "/src/",
+    publicPath: '/src/',
+    filename: "scripts.min.js"
+  },
+  plugins: debug ? []: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+};
